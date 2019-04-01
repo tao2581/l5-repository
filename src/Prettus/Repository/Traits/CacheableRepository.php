6a02ea006f413106c505e272f233d14a7ca633ff
tao2581 <tao2581@126.com>
2019-04-01 13:39:39 +0800

    Update CacheableRepository.php

--- src/Prettus/Repository/Traits/CacheableRepository.php
+++ src/Prettus/Repository/Traits/CacheableRepository.php
@@ -219,7 +219,7 @@ trait CacheableRepository
      *
      * @return mixed
      */
-    public function paginate($limit = null, $columns = ['*'])
+    public function paginate($limit = null, $columns = ['*'], $method = "paginate")
     {
         if (!$this->allowedCache('paginate') || $this->isSkippedCache()) {
             return parent::paginate($limit, $columns);
